dae4d4705cd4e139041da1fb3aaa7396c0396d17
Luis Antonio Preza Padilla <anibulusnn@gmail.com>
2021-10-17 00:30:55 -0500

    Cambio en mÃtodo de bÃsqueda
    Realiza el muestreo de la infromaciÃn y aÃade el ciclo a la secciÃn de
    la resoluciÃn del problema para una busqueda intensiva

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 EXIT = 'exit'
+TRIES = 3
 
 def get_num(msj):
     num = ''
@@ -17,7 +18,7 @@ def discard_options(words_list, word, likely=0):
     Recorre la palabra para comparar las letras en sus respectivas posiciones
     """
     passw_posibilities = []
-
+    aux_list = []
 
     for passw in words_list:
         if not passw.__eq__(word):
@@ -27,17 +28,18 @@ def discard_options(words_list, word, likely=0):
             
             coincidences = len(passw_char)
             if coincidences == likely:
+                aux_list.append(passw)
                 passw_posibilities.append({"password":passw, "coincidences":coincidences, "on_letter":passw_char})
 
-    print(passw_posibilities)
+    if len(aux_list) > 0:
+        print(f'Estas son las posibles palabras con {likely} coincidencias')
+        for item in passw_posibilities:
+            print(f'Palabra: {item.get("password").capitalize()} en la letra {item.get("on_letter")}')
+    else:
+        print('No hay concidencias')
     
-    """
-    filter recibe una funcion anonima y un iterable
-    options = list(filter(lambda x : x not in words_list, words_list))
-    options = [x for x in words_list if x not in new_list]
-    opc = list(map(lambda x : x*2, words_list))
-    Ver Reduce
-    """
+    words_list = aux_list
+    return words_list
 
 
 def run():
@@ -46,31 +48,28 @@ def run():
 
     get_words_from_user = False
     while get_words_from_user:
-
         data_input = get_word()
-
         #Cuando sea el comando de salida, prepara a terminar la ejecución
         if (data_input.__eq__(f'\\{EXIT}')):
             get_words_from_user = False
         else:
-            #Antes de agregarla, revisa si la palabra ya wxiste en la lista
+            #Antes de agregarla, revisa si la palabra ya existe en la lista
             if not data_input in words_list:
                 words_list.append(data_input)     
             else:
-                print(f'La palabra "{data_input}" ya ha sido agregada')
+                print(f'La palabra "{data_input.capitalize()}" ya ha sido agregada')
 
-
-    #TODO datos de prueba
-    words_list=['down', 'upon', 'here', 'stay', 'star', 'block', 'take', 'ride', 'there', ]
-    print(words_list)
-    #Una vez terminado el ciclo de captura los intentos
-    word = get_word('¿Cuál palabra has intentado?')
-    if word in words_list:
-        likely = get_num('Interta la similitud \t')
-        #Descarta todas las opciones que contengan sus letras
-        discard_options(words_list, word, likely)
-    else:
-        print('Esa palabra no está agregada')
+    counter = 1
+    
+    while (len(words_list) > 1) and (counter < TRIES):
+        word = get_word('¿Cuál palabra has intentado?')
+        if word in words_list:
+            likely = get_num('Inserta las similitudes \t')
+            #Descarta todas las opciones que contengan sus letras
+            words_list = discard_options(words_list, word, likely)
+            counter += 1
+        else:
+            print('Esa palabra no está agregada')
 
 
 if __name__ == "__main__":
